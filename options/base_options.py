eca58d050e34872852736a5dd5f32bf01feed343
Chan-man00 <113739959+Chan-man00@users.noreply.github.com>
2024-04-21 16:48:54 -0600

    Add files via upload
    added some of the missing files that I had locally.

--- options/base_options.py
+++ options/base_options.py
@@ -37,6 +37,7 @@ class BaseOptions():
         parser.add_argument('--init_type', type=str, default='normal', help='network initialization [normal | xavier | kaiming | orthogonal]')
         parser.add_argument('--init_gain', type=float, default=0.02, help='scaling factor for normal, xavier and orthogonal.')
         parser.add_argument('--no_dropout', action='store_true', help='no dropout for the generator')
+        #parser.add_argument('--dropout_rate', type=float, default=0.5, help='the dropout rate')
         # dataset parameters
         parser.add_argument('--dataset_mode', type=str, default='unaligned', help='chooses how datasets are loaded. [unaligned | aligned | single | colorization]')
         parser.add_argument('--direction', type=str, default='AtoB', help='AtoB or BtoA')
--- options/train_options.py
+++ options/train_options.py
@@ -35,6 +35,8 @@ class TrainOptions(BaseOptions):
         parser.add_argument('--pool_size', type=int, default=50, help='the size of image buffer that stores previously generated images')
         parser.add_argument('--lr_policy', type=str, default='linear', help='learning rate policy. [linear | step | plateau | cosine]')
         parser.add_argument('--lr_decay_iters', type=int, default=50, help='multiply by a gamma every lr_decay_iters iterations')
+        # added dropout rate.
+        parser.add_argument('--dropout_rate', type=float, default=0.5, help='the dropout rate')
 
         self.isTrain = True
         return parser
